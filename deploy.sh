7e01e316cd730b6044337c83be0262a6171a9c58
jiapengfei <felix9ia@outlook.com>
2021-02-19 10:30:19 +0800

    记录自定义domain，尝试微信可访问支持

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,13 @@ set -e
 # 生成静态文件
 #npm run docs:build
 npm run build
-
+# 支持微信可访问
+cp ./MP_verify_9RvbxRWFvPhDUHV6.txt ./dist
 # 进入生成的文件夹
 cd ./dist
 
 # 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+echo 'blog.medalwall.top' > CNAME
 
 git init
 git add -A
@@ -19,6 +20,6 @@ git commit -m 'deploy'
 
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/felix9ia/sloth.git  master:gh-pages
+git push -f https://github.com/felix9ia/felix9ia.github.io.git  master:gh-pages
 
 cd -
