30aa4f8c78a1e689b6cb302bb20346788f9e9609
jiapengfei <felix9ia@outlook.com>
2021-03-11 13:54:39 +0800

    backup

---
+++ src/computer/structures_algorithm/code/kthLargestElement_5.go
@@ -0,0 +1,16 @@
+package main
+
+/**
+ * @param n: An integer
+ * @param nums: An array
+ * @return: the Kth largest element
+ */
+func kthLargestElement(n int, nums []int) int {
+	// write your code here
+	return 0
+}
+
+func main() {
+	var arr1 = []int{3, 1, 9, 4}
+	kthLargestElement(3, arr1)
+}
