04ca85dcd718518f73e8034b341e606b21a3dddd
jiapengfei <felix9ia@outlook.com>
2021-03-13 13:48:57 +0800

    有思路,没状态.  TODO

---
+++ src/computer/structures_algorithm/code/TreenodeDeSe.java
@@ -0,0 +1,28 @@
+
+public class TreeNode {
+    public int val;
+    public TreeNode left,right;
+    public TreeNode(int val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
+
+public class Solution {
+
+    public String serialize(TreeNode root) {
+
+    }
+
+    public TreeNode deserialize(String data) {
+
+    }
+
+    public static void main() {
+        Solution solution = new Solution();
+        solution.serialize()
+    }
+
+
+}
