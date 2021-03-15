352e041d9897d6b8b46bfa2dbe0de500ebed582e
jiapengfei <felix9ia@outlook.com>
2021-03-15 13:59:58 +0800

    看答案抄下来的,算是第一道通过测试编译的...

--- src/computer/structures_algorithm/code/rotateString_8.py
+++ src/computer/structures_algorithm/code/rotateString_8.py
@@ -4,5 +4,14 @@ class Solution:
     @param offset: An integer
     @return: nothing
     """
-    def rotateString(self, s, offset):
+    def rotateString(self, str, offset):
         # write your code here
+        if not offset: return
+        if not str: return
+
+        offset = offset % len(str)
+        for i in range(offset)
+            c = str.pop()
+            str.insert(0, c)
+
+        return str
